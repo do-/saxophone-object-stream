71c3a858c8626b08d9e25707d872e1c6025d12ed
Dmitry Ovsyanko <do_@pochta.ru>
2021-11-06 10:48:15 +0300

    +noText

--- test/test.js
+++ test/test.js
@@ -12,12 +12,17 @@ async function test_list () {
 	let get_is = () => fs.readFileSync (FN_LIST)
 	
 	let l = await XMLReader.list (get_is (), {
+/*
 		fieldMap: {
 			ID: 'id',
 			NAME: 'label',
 			ISACTIVE: 'is_active',
     	},
-		filter: i => i.is_active === 'true',
+*/
+//		filter: i => i.is_active === 'true',
+		noText: true,
+//		noAttributes: true,
+//		wrap: true,
 	})
 
 console.log (l)
@@ -37,6 +42,7 @@ async function test_soap () {
 //		nsPrefixes: 'use',
 //		nsPrefixes: 'ignore',
 //		noAttributes: true,
+//		noText: true,
 //		localName: 'SenderProvidedRequestData',
 //		nameSpace: 'urn://x-artefacts-smev-gov-ru/services/message-exchange/types/1.1',
 	})
@@ -48,8 +54,8 @@ async function main () {
 
 	let not_now = Date.now ()
 
-	await test_list () 
-//	await test_soap () 
+//	await test_list () 
+	await test_soap () 
 
 console.log ((Date.now () - not_now) + ' ms')
 
