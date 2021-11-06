49825ddb542af0445fc35d7d262008645619fcb7
Dmitry Ovsyanko <do_@pochta.ru>
2021-11-06 10:37:07 +0300

    minor optimizations

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ async function test_list () {
 //	let get_is = () => fs.createReadStream (FN_LIST)
 	let get_is = () => fs.readFileSync (FN_LIST)
 	
-	let l = await XMLReader.get (get_is (), {
+	let l = await XMLReader.list (get_is (), {
 		fieldMap: {
 			ID: 'id',
 			NAME: 'label',
@@ -31,13 +31,13 @@ async function test_soap () {
 	let src = get_is ()
 	
 	let l = await XMLReader.get (src, {
-		level: 3,
-//		wrap: true,
+		level: 0,
+		wrap: true,
 //		filter: i => i.Id,
 //		nsPrefixes: 'use',
 //		nsPrefixes: 'ignore',
 //		noAttributes: true,
-		localName: 'SenderProvidedRequestData',
+//		localName: 'SenderProvidedRequestData',
 //		nameSpace: 'urn://x-artefacts-smev-gov-ru/services/message-exchange/types/1.1',
 	})
 console.log (JSON.stringify (l, null, 2))
@@ -48,8 +48,8 @@ async function main () {
 
 	let not_now = Date.now ()
 
-//	await test_list () 
-	await test_soap () 
+	await test_list () 
+//	await test_soap () 
 
 console.log ((Date.now () - not_now) + ' ms')
 
